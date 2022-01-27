d956691fa31b94680017fa5ad301c8178838dd67
iatenothingbutriceforthreedays <darwinvickers@gmail.COM>
2022-01-27 17:21:44 -0500

    dialogue stub

---
+++ Assets/ThirdPerson/Dialogue/DialogueManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System;
+
+public class DialogueManager : MonoBehaviour
+{
+    public PlayerInput input;
+   //public Action OnUnfreeze;
+
+    public RawImage characterFaceUI; // the panel that shows the person's face as they are speaking
+
+    private bool _busy;
+
+    private bool _isTalkAvailable = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTalkAvailable(bool b) {
+      _isTalkAvailable = b;
+    }
+    public bool IsTalkAvailable() {
+      return _isTalkAvailable;
+    }
+
+    public void StartDialogue(string fungusMessage, Texture faceTexture) {
+      Freeze();
+      _busy = true;
+
+      if(characterFaceUI != null) characterFaceUI.texture = faceTexture;
+
+      // Tell fungus dialog to start
+      // Fungus.Flowchart.BroadcastFungusMessage(fungusMessage);
+      // TODO: port to YarnSpinner
+    }
+
+    private void Freeze() {
+      // freeze player character.
+      if (input != null) input.enabled = false;
+
+      Debug.Log("Freeze");
+    }   
+    public void DialogueEnd() {
+      _busy = false;
+      // unfreeze player character.
+      if (input != null) input.enabled = true;
+      Debug.Log("Unfreeze");
+      //OnUnfreeze();
+    }
+
+    public bool IsBusy() {
+      return _busy;
+    }
+}
---
+++ Assets/ThirdPerson/Dialogue/NPCDialogue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEditor;
+
+[RequireComponent(typeof(Collider))]
+public class NPCDialogue : MonoBehaviour {
+  public string talkButton = "Fire1"; // TODO replace with proper input thing
+
+  public string dialogueMessage = "KnightSpeak";
+
+  public Texture characterPreview;
+
+  public GameObject talkable;
+
+  private bool _canTalk = false;
+
+  public DialogueManager dm;
+
+  // Use this for initialization
+  void Start () {
+    if (talkable) talkable.SetActive(false);
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    if (_canTalk && !dm.IsBusy() && Input.GetButtonDown(talkButton)) {
+      Debug.Log("start dialog "+dialogueMessage);
+
+      dm.StartDialogue(dialogueMessage, characterPreview);
+    }
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.CompareTag("DialogueTarget") && !dm.IsTalkAvailable() && !dm.IsBusy()) {
+      _canTalk = true;
+      dm.SetTalkAvailable(true);
+      if (talkable) talkable.SetActive(true);
+    }
+  }
+
+  void OnTriggerExit(Collider other) {
+    if (other.CompareTag("DialogueTarget") && _canTalk) {
+      _canTalk = false;
+      dm.SetTalkAvailable(false);
+      if (talkable) talkable.SetActive(false);
+    }
+  }
+
+
+}
