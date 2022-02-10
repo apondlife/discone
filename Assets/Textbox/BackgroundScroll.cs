36c2e05df59c3662d0074d8f51ab198379d29b48
fran <franmorojo@gmail.com>
2022-02-10 17:21:16 -0500

    scrollll

--- Assets/Textbox/BackgroundScroll.cs
+++ Assets/Textbox/BackgroundScroll.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackgroundScroll : MonoBehaviour
 {
 
     CanvasRenderer rend;
+    RawImage img;
     public float scrollSpeed = .3f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<CanvasRenderer>();
+
+        img = GetComponent<RawImage>();
         Debug.Log(rend);
         
     }
@@ -19,12 +25,21 @@ public class BackgroundScroll : MonoBehaviour
     void Update()
     {
 
-        float offset = Time.time * scrollSpeed;
+        // float offset = Time.time * scrollSpeed;
 
-        var mat = rend.GetMaterial();
-        if (mat) {
-            rend.GetMaterial().SetTextureOffset("_MainTex", new Vector2(-offset, offset));
-        }
+        
+
+        // var mat = rend.GetMaterial();
+        // if (mat) {
+        //     rend.GetMaterial().SetTextureOffset("_MainTex", new Vector2(-offset, offset));
+        // }
+
+
+        float newX = img.uvRect.x + scrollSpeed;
+        float newY = img.uvRect.y + scrollSpeed;
+        img.uvRect = new Rect(newX, newY, img.uvRect.width, img.uvRect.height);
+
+        Debug.Log(newX);
         
     }
 }
