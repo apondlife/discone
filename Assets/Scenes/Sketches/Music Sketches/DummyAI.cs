6be4b288349e0de2ea5cf35336adc6e911898232
iatenothingbutriceforthreedays <darwinvickers@gmail.COM>
2022-02-23 15:54:38 -0500

    fairly broken scene

--- Assets/Scenes/Sketches/Music Sketches/DummyAI.cs
+++ Assets/Scenes/Sketches/Music Sketches/DummyAI.cs
@@ -7,11 +7,14 @@ using ThirdPerson;
 [System.Serializable]
 public class DummyAI : ThirdPerson.InputProvider {
     // -- fields --
+    [Tooltip("input direction")]
+    [SerializeField] private Vector2 inputDirection = new Vector2(0.7f, 0.7f);
+
     [Tooltip("probability of jumping per frame")]
     [SerializeField] private float jumpProbability = 0.001f;
 
     public override Vector2 Move {
-        get => Vector2.right; // always move right
+        get => inputDirection;
     }
 
     public override bool IsJumpPressed {
