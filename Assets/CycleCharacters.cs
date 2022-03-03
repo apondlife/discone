79e144746b2493b4ddc2b05660a34cc13d6cbcfe
mutmedia <gustavo.ceci95@gmail.com>
2022-03-02 19:14:49 -0500

    cycle characters

--- Assets/CycleCharacters.cs
+++ Assets/CycleCharacters.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ThirdPerson;
 
 public class CycleCharacters : MonoBehaviour
 {
+    private Player player;
+    private ThirdPerson.ThirdPerson[] characters;
+    private int current = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characters = FindObjectsOfType<ThirdPerson.ThirdPerson>();
+        player = GetComponentInParent<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Cycle() {
+        current = (current + 1) % characters.Length;
+        player.Drive(characters[current]);
     }
 }
