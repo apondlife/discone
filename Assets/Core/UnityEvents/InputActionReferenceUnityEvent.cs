c62fa4ec0feff5388ae3fc6c1a350b4a371e9b64
mutmedia <gustavo.ceci95@gmail.com>
2022-03-10 02:01:26 -0500

    cycle and some worldstuff

---
+++ Assets/Core/UnityEvents/InputActionReferenceUnityEvent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+namespace MutCommon
+{
+  public class InputActionReferenceUnityEvent : MonoBehaviour
+  {
+    public enum KeyCodeType
+    {
+      Down,
+      Hold,
+      Up,
+    }
+
+    [Serializable]
+    public class KeyCodeTypeEvent
+    {
+      public KeyCode Key;
+      public KeyCodeType Type;
+      public UnityEvent Event;
+    }
+
+    [SerializeField] private InputActionReference InputAction;
+    [SerializeField] public KeyCodeType Type;
+    [SerializeField] public UnityEvent Event;
+
+    void Update()
+    {
+      switch (Type)
+      {
+        case KeyCodeType.Down:
+          if (InputAction.action.WasPressedThisFrame()) Event.Invoke();
+          break;
+        case KeyCodeType.Hold:
+          if (InputAction.action.IsPressed()) Event.Invoke();
+          break;
+        case KeyCodeType.Up:
+          if (InputAction.action.WasReleasedThisFrame()) Event.Invoke();
+          break;
+      }
+    }
+  }
+}
