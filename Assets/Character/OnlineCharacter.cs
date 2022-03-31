9b85b4c1615acbf9a945bd0682e1120dd3bb33f4
ty cobb <bin@tycobb.club>
2022-03-31 10:38:47 -0400

    Revert "wip sync state"
    This reverts commit e3da01533c85c157b4cc23fa000ba283fc0dc361.

--- Assets/Character/OnlineCharacter.cs
+++ Assets/Character/OnlineCharacter.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
 using Mirror;
-using ThirdPerson;
 
 /// an online character
-[RequireComponent(typeof(Character))]
 sealed class OnlineCharacter: NetworkBehaviour {
     // -- constants --
-    /// a parent "folder" for the characters
     static Transform k_Characters;
 
     // -- fields --
@@ -18,53 +15,24 @@ sealed class OnlineCharacter: NetworkBehaviour {
     [Tooltip("if the character is currently available")]
     [SyncVar] [SerializeField] bool m_IsAvailable = true;
 
-    // -- props --
-    /// the underlying character
-    Character m_Character;
-
-    /// the character's most recent state frame
-    [SyncVar] CharacterState.Frame m_CurrentState;
-
     // -- lifecycle --
     void Awake() {
-        // set deps
-        m_Character = GetComponent<Character>();
-
-        // debug
         #if UNITY_EDITOR
-        // create shared characters go
+        // create shared characters "folder"
         if (k_Characters == null) {
             var obj = new GameObject();
             obj.name = "Characters";
             k_Characters = obj.transform;
         }
 
-        // move character to parent
+        // move character to folder
         transform.parent = k_Characters.transform;
         #endif
     }
 
-    void FixedUpdate() {
-        // if we have authority, push state
-        if (netIdentity.hasAuthority) {
-            Debug.Log($"[online] char {name} - push state: {!m_Character.CurrentState.Equals(m_CurrentState)}");
-            m_CurrentState = m_Character.CurrentState;
-        }
-        // otherwise, apply whatever was synced
-        // NOTE: we guard null b/c the first frame FixedUpdate is called, after OnStartServer &
-        // RemoveClientAuthority are called, netIdentity.hasAuthority is false and m_CurrentState
-        // is null. there's probably a less hacky way to detect this state.
-        else if (m_CurrentState != null) {
-            Debug.Log($"[online] char {name} - receive state");
-            m_Character.ForceState(m_CurrentState);
-        }
-    }
-
-    // -- l/mirror
     public override void OnStartServer() {
         base.OnStartServer();
 
-        Debug.Log($"start server");
         // initially, nobody has authority over any character (except the host client)
         RemoveClientAuthority();
     }
