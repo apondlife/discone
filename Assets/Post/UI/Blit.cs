6266a273f4ff4f2508de6ba84701c5015ed5df84
fran <franmorojo@gmail.com>
2022-04-11 19:52:43 -0400

    new ui who dis

--- Assets/Post/UI/Blit.cs
+++ Assets/Post/UI/Blit.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 /// blit a post processing effect
 [ExecuteInEditMode]
@@ -9,10 +10,30 @@ public class Blit: MonoBehaviour {
     [Tooltip("the post-processing material (shader)")]
     [SerializeField] Material m_Material;
 
+    [SerializeField] float dissolveAmmount;
+
     // -- lifecycle --
     void Awake() {
         // make sure we have the depth & normals texture
         var camera = GetComponent<Camera>();
+
+        // // create new command buffer
+        // var buffer = new CommandBuffer();
+        // buffer.name = "frans buffer";
+        // // m_Cameras[cam] = m_GlowBuffer;
+
+        // int tempID = Shader.PropertyToID("_fransTempRT");
+        // buffer.GetTemporaryRT(tempID, -1, -1);
+        // buffer.SetRenderTarget(tempID);
+        // buffer.ClearRenderTarget(true, true, Color.red); // clear before drawing to it each frame!!
+
+
+        // camera.AddCommandBuffer(CameraEvent.BeforeImageEffects, buffer);
+    }
+
+    void Update() {
+        m_Material.SetFloat("_DissolveAmmount", dissolveAmmount);
+
     }
 
     void OnRenderImage(RenderTexture src, RenderTexture dst) {
