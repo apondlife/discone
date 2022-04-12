595a45561b554eaf19d104448ecb3f5b5dae41ea
fran <franmorojo@gmail.com>
2022-04-12 17:14:17 -0400

    testing lerping

--- Assets/UI/RegionSign/RegionSign.cs
+++ Assets/UI/RegionSign/RegionSign.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using MutCommon;
 
 public class RegionSign : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class RegionSign : MonoBehaviour
 
     [SerializeField]
     internal TextMeshProUGUI m_Text;
+
+    private float testK = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +23,19 @@ public class RegionSign : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+          Debug.Log(testK);
+    }
+
+    void TestAction(float k) {
+        testK = k;
     }
 
     public void OnRegionEntered(string regionName) {
         canvasGroup.alpha = 1;
+        StartCoroutine(CoroutineHelpers.InterpolateByTime(1f, TestAction));
+      
+        // funciton that recives a k goes from 0 to 1
+        // 3rd param takes that k and does somethign with it
         m_Text.SetText(regionName);
     }
 }
