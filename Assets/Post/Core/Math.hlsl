9b79db470f979febd0f60511b13b8e08742be2f8
mutmedia <gustavo.ceci95@gmail.com>
2022-04-27 23:23:47 -0400

    fuzz as post processing

---
+++ Assets/Post/Core/Math.hlsl
@@ -0,0 +1,15 @@
+// -- defines --
+#ifndef float1
+#define float1 float
+#endif
+
+// -- functions --
+/// inverse lerp a value (v) given a min (a) and max (b): (a, b) -> (0, 1)
+float3 Unlerp(float a, float b, float v) {
+    return (v - a) / (b - a);
+}
+
+/// sample a random value, between 0 and 1, for a 2d coordinate
+float Rand(float2 st) {
+    return frac(sin(dot(st, float2(12.9898f, 78.233f))) * 43758.5453123f);
+}
