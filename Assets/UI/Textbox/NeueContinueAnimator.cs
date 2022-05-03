09123bfdd30baeef521165ba67ac8f87d8c372cc
fran <franmorojo@gmail.com>
2022-05-03 19:22:20 -0400

    default textbox, except for ivan

--- Assets/UI/Textbox/NeueContinueAnimator.cs
+++ Assets/UI/Textbox/NeueContinueAnimator.cs
@@ -20,22 +20,17 @@ public class NeueContinueAnimator : MonoBehaviour
     }
 
     IEnumerator AnimateElipsis() {
-        Debug.Log("started animated elipsis coroutine");
-        
 
         //lineText.ForceMeshUpdate();
         TMP_TextInfo textInfo = m_Text.textInfo;
         int characterCount = 3; // m_Text.textInfo.characterCount;
         numVisible = characterCount;
-        Debug.Log(characterCount);
 
         while (true) {
             numVisible = (numVisible + 1) % characterCount;
             m_Text.maxVisibleCharacters = numVisible + 1;
             m_Text.ForceMeshUpdate();
-            // Debug.Log("visible...");
-            // Debug.Log(numVisible);
-            // Debug.Log(m_Text.maxVisibleCharacters);
+
             yield return new WaitForSecondsRealtime(0.5f);
         }
     }
