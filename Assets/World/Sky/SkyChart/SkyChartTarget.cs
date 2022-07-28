2123b4d2cfe5e8b624c11199c29bf83f6683aad6
ty cobb <bin@tycobb.club>
2022-07-28 13:42:11 -0400

    targets spawn bodies directly

--- Assets/World/Sky/SkyChart/SkyChartTarget.cs
+++ Assets/World/Sky/SkyChart/SkyChartTarget.cs
@@ -3,14 +3,9 @@ using UnityAtoms;
 using UnityAtoms.BaseAtoms;
 
 /// the sky-based navigation system
-[ExecuteAlways]
-[RequireComponent(typeof(SkyChartBody))]
 class SkyChartTarget: MonoBehaviour {
     // -- config --
     [Header("config")]
-    [Tooltip("the target object")]
-    [SerializeField] Transform m_Target;
-
     [Tooltip("the zenith of the object when we are far from it")]
     [UnityEngine.Range(-90.0f, 90.0f)]
     [SerializeField] float m_FarZenith;
@@ -19,22 +14,34 @@ class SkyChartTarget: MonoBehaviour {
     [UnityEngine.Range(-90.0f, 90.0f)]
     [SerializeField] float m_CloseZenith;
 
+    [Tooltip("the prefab for the body")]
+    [SerializeField] GameObject m_BodyPrefab;
+
     // -- refs --
     [Header("refs")]
     [Tooltip("the player")]
     [SerializeField] DisconePlayerVariable m_Player;
 
+    [Tooltip("the parent for sky chart bodies")]
+    [SerializeField] GameObjectVariable m_Bodies;
 
     // -- props --
     /// the celestial body
     SkyChartBody m_Body;
-    void Awake() {
-      m_Body = GetComponent<SkyChartBody>();
-    }
 
     // -- lifeycle --
+    void Awake() {
+        // create the body in the sky
+        var body = Instantiate(
+            m_BodyPrefab,
+            m_Bodies.GetComponent<Transform>()
+        );
+
+        m_Body = body.GetComponent<SkyChartBody>();
+    }
+
     void FixedUpdate() {
-        var pt = m_Target.transform.position;
+        var pt = transform.position;
         var pc = m_Player.Value.transform.position;
 
         // get delta to the target
@@ -46,16 +53,19 @@ class SkyChartTarget: MonoBehaviour {
 
         // calculate new coordinate
         var coord = m_Body.Coordinate;
-        coord.Azimuth = Vector3.SignedAngle(dir, Vector3.left, Vector3.up);
-        coord.Zenith = ZenithCurve(dist);
+        coord.Azimuth = Vector3.SignedAngle(
+            dir,
+            Vector3.left,
+            Vector3.up
+        );
 
+        coord.Zenith = Mathf.Lerp(
+            m_CloseZenith,
+            m_FarZenith,
+            2.0f * Mathf.Atan(dist) / Mathf.PI
+        );
+;
         // update body
         m_Body.Coordinate = coord;
-    }
-
-    // zenith angle given the distance
-    float ZenithCurve(float x) {
-      var k =  2* Mathf.Atan(x) / Mathf.PI;
-      return Mathf.Lerp(m_CloseZenith, m_FarZenith, k);
     }
 }
