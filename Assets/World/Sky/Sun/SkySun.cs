670781eabf63409415924fba893011f2a3ceb51e
ty cobb <bin@tycobb.club>
2022-08-04 17:16:39 -0400

    add more sun/moon; sky camera rot syncing seems behind player cam

--- Assets/World/Sky/Sun/SkySun.cs
+++ Assets/World/Sky/Sun/SkySun.cs
@@ -1,22 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityAtoms.BaseAtoms;
 
 /// a sun that changes color
 public class SkySun: MonoBehaviour {
+    // -- statics --
+    /// the cache of per-color materials
+    static Dictionary<string, Material> s_MaterialCache = new Dictionary<string, Material>();
+
     // -- config --
     [Header("config")]
-    [Tooltip("the sun color when hosting a server")]
+    [Tooltip("the sun color when off")]
     [ColorUsage(true, true)]
-    [SerializeField] Color m_HostColor;
+    [UnityEngine.Serialization.FormerlySerializedAs("m_HostColor")]
+    [SerializeField] Color m_OffColor;
 
-    [Tooltip("the sun color when connected as a client")]
+    [Tooltip("the sun color when on")]
     [ColorUsage(true, true)]
-    [SerializeField] Color m_ClientColor;
+    [UnityEngine.Serialization.FormerlySerializedAs("m_ClientColor")]
+    [SerializeField] Color m_OnColor;
 
     // -- refs --
     [Header("refs")]
-    [Tooltip("if the player is the host")]
-    [SerializeField] BoolVariable m_IsHost;
+    [Tooltip("the number of players")]
+    [SerializeField] IntVariable m_PlayerCount;
 
     // -- props --
     /// the list of renderers
@@ -30,8 +37,12 @@ public class SkySun: MonoBehaviour {
         // set props
         m_Renderers = GetComponentsInChildren<Renderer>();
 
+        // set current state
+        SyncColor();
+
         // bind events
-        m_Subscriptions.Add(m_IsHost.Changed, OnIsHostChanged);
+        m_Subscriptions
+            .Add(m_PlayerCount.Changed, OnPlayerCountChanged);
     }
 
     void OnDestroy() {
@@ -39,13 +50,37 @@ public class SkySun: MonoBehaviour {
         m_Subscriptions.Dispose();
     }
 
+    // -- commands --
+    /// sync color with current state
+    void SyncColor() {
+        // get current color
+        var count = m_PlayerCount.Value;
+        var color = count <= 1 ? m_OffColor : m_OnColor;
+
+        // update materials
+        foreach (var r in m_Renderers) {
+            r.material = FindInstance(r.sharedMaterial, color);
+        }
+    }
+
+    // -- queries --
+    /// find cached material for texture and saturation
+    Material FindInstance(Material mat, Color color) {
+        var key = $"{mat.name}/{color.r}:{color.g}:{color.b}";
+
+        // create instanced material for the texture if not cached
+        if (!s_MaterialCache.TryGetValue(key, out var material)) {
+            material = Instantiate(mat);
+            material.color = color;
+            s_MaterialCache.Add(key, material);
+        }
+
+        return material;
+    }
+
     // -- events --
     /// when the player switches between host/client
-    void OnIsHostChanged(bool isHost) {
-        // change to the correct color
-        var c = isHost ? m_HostColor : m_ClientColor;
-        foreach(var r in m_Renderers) {
-            r.sharedMaterial.color = c;
-        }
+    void OnPlayerCountChanged(int count) {
+        SyncColor();
     }
 }
