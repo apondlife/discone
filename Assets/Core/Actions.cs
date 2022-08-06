26f8550d9975e4d53901ae81a93832b33666fe00
ty cobb <bin@tycobb.club>
2022-08-06 18:23:23 -0400

    hook up field to chunk events; be careful with task threading

--- Assets/Core/Actions.cs
+++ Assets/Core/Actions.cs
@@ -13,22 +13,19 @@ static class Actions {
         var curr = null as CancellationTokenSource;
 
         // create an action that cancels the previous task when invoked
-        return () => {
+        return async () => {
             // cancel prev task
             curr?.Cancel();
             curr = new CancellationTokenSource();
 
-            // start the new task (_ is only to avoid weird 4 char alignment)
-            var _ = Task
-                .Delay(millis, curr.Token)
-                .ContinueWith(
-                    (t) => {
-                        if (t.IsCompletedSuccessfully) {
-                            action();
-                        }
-                    },
-                    TaskScheduler.Default
-                );
+            // create a new delay
+            var task = Task.Delay(millis, curr.Token);
+
+            // if it completes, call the action
+            await task;
+            if (task.IsCompletedSuccessfully) {
+                action();
+            }
         };
     }
 }
--- Assets/World/Field/Field.cs
+++ Assets/World/Field/Field.cs
@@ -1,19 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 using UnityAtoms;
 using UnityAtoms.BaseAtoms;
 
 /// an infinite field
 [ExecuteAlways]
 public sealed class Field: MonoBehaviour {
-    // -- constants --
-    /// the maximum number of active chunks
-    private const int k_MaxChunks = 16;
-
-    /// the duration between purges
-    private const float k_PurgeChunksInterval = 2.0f;
-
     // -- state --
     [Header("state")]
     [Tooltip("the size of a world chunk")]
@@ -21,10 +13,6 @@ public sealed class Field: MonoBehaviour {
 
     // -- config --
     [Header("config")]
-    [Tooltip("the player to follow")]
-    [SerializeField] DisconePlayerVariable m_Player;
-
-    [UnityEngine.Serialization.FormerlySerializedAs("m_Terrain")]
     [Tooltip("the prefab for creating chunks")]
     [SerializeField] FieldChunk m_Chunk;
 
@@ -49,69 +37,56 @@ public sealed class Field: MonoBehaviour {
     [Tooltip("the maximum elevation amount")]
     [SerializeField] float m_MaxElevation = 20.0f;
 
-    // -- props --
-    /// the target's current coordinate. the current center chunk index
-    Vector2Int m_TargetCoord = new Vector2Int(69, 420);
+    // -- events --
+    [Header("events")]
+    [Tooltip("when a chunk loads")]
+    [SerializeField] Vector2IntEvent m_LoadedChunk;
 
+    [Tooltip("when a chunk unloads")]
+    [SerializeField] Vector2IntEvent m_UnloadedChunk;
+
+    // -- props --
     /// the map of visible chunks
     Dictionary<Vector2Int, FieldChunk> m_Chunks = new Dictionary<Vector2Int, FieldChunk>();
 
     /// a pool of free chunk instances
     Queue<FieldChunk> m_ChunkPool = new Queue<FieldChunk>();
 
+    /// the set of event subscriptions
+    Subscriptions m_Subscriptions = new Subscriptions();
+
     // -- lifecycle --
-    void Awake() {
-        // capture chunk size
-        m_ChunkSize.Value = m_Chunk.Size.x;
-
-        // dont persist changes to the editor
-        // m_FieldHeight = m_FieldHeight.Unsaved();
-    }
-
-    void Start() {
+    void Init() {
         #if UNITY_EDITOR
+        // destroy any editor terrain
+        ClearEditorChunks();
+        #endif
+
         // capture chunk size
         Debug.Assert(m_Chunk.Size.x == m_Chunk.Size.z, "field's terrain chunk was not square");
         m_ChunkSize.Value = m_Chunk.Size.x;
 
-        // destroy any editor terrain
-        ClearEditorChunks();
+        // dont persist changes to the editor
+        // m_FieldHeight = m_FieldHeight.Unsaved();
 
-        // if editor, don't do anything else
-        if (!Application.IsPlaying(gameObject)) {
-            return;
-        }
-        #endif
+        // bind events
+        m_Subscriptions
+            .Add(m_LoadedChunk, CreateChunk)
+            .Add(m_UnloadedChunk, DestroyChunk);
+    }
 
-        // start purge routine
-        StartCoroutine(Coroutines.Interval(k_PurgeChunksInterval, PurgeChunks));
+    void Awake() {
+        Init();
     }
 
     #if UNITY_EDITOR
-    void Update() {
-        // if editor, create editor chunks
+    void Start() {
+        // if editor, don't do anything else
         if (!Application.IsPlaying(gameObject)) {
-            CreateEditorChunks();
-            return;
+            Init();
         }
     }
     #endif
-
-    void FixedUpdate() {
-        // if the target is active
-        if (!m_Player.Value) {
-            return;
-        }
-
-        var tt = m_Player.Value.transform;
-
-        // if the target changed chunks, create neighbors
-        var coord = IntoCoordinate(tt.position);
-        if (m_TargetCoord != coord) {
-            m_TargetCoord = coord;
-            CreateChunks();
-        }
-    }
 
     void OnValidate () {
         m_FieldHeight.SetFloat("_FloorScale", m_FloorScale);
@@ -124,23 +99,11 @@ public sealed class Field: MonoBehaviour {
         ReloadEditorChunks();
     }
 
-    // -- commands --
-    // -- c/create
-    /// create new chunks as the player moves
-    void CreateChunks(int size = 3) {
-        // the number of chunks to create
-        var n = size * size;
-
-        // instantiate a square of 9 chunks around the target
-        for (var i = 0; i < n; i++) {
-            var c = m_TargetCoord;
-            c.x += i % size - 1;
-            c.y += i / size - 1;
-
-            CreateChunk(c);
-        }
+    void OnDestroy() {
+        m_Subscriptions.Dispose();
     }
 
+    // -- commands --
     /// create a new chunk at a coordinate if necessary
     void CreateChunk(Vector2Int coord) {
         // if the chunk already exists, don't create one
@@ -150,7 +113,7 @@ public sealed class Field: MonoBehaviour {
 
         // add the chunk to the map of active chunks
         var chunk = DequeueChunk();
-        m_Chunks.Add(coord, chunk);
+        m_Chunks[coord] = chunk;
 
         // load the chunk for this coordinate
         chunk.Load(coord);
@@ -165,7 +128,24 @@ public sealed class Field: MonoBehaviour {
 
         // move the chunk into position
         var tc = chunk.transform;
-        tc.localPosition = IntoPosition(coord);
+        tc.localPosition = WorldCoord.IntoPosition(coord, m_ChunkSize.Value);
+    }
+
+    /// destroy the chunk at this coordinate if necessary
+    void DestroyChunk(Vector2Int coord) {
+        // if it doesn't exist, do nothing
+        if (!m_Chunks.ContainsKey(coord)) {
+            return;
+        }
+
+        // turn off the chunk
+        var chunk = m_Chunks[coord];
+        chunk.gameObject.SetActive(false);
+        m_Chunks.Remove(coord);
+
+        // and add it back to the pool
+        // TODO: maybe the pool should also be size limited
+        m_ChunkPool.Enqueue(chunk);
     }
 
     /// dequeue a terrain chunk from the pool
@@ -186,133 +166,25 @@ public sealed class Field: MonoBehaviour {
         return chunk;
     }
 
-    // -- c/purge
-    /// purge any unused chunks
-    void PurgeChunks() {
-        if (m_Chunks.Count <= k_MaxChunks) {
-            return;
-        }
-
-        // remove the chunks that are further away and no longer needed
-        var t = m_TargetCoord;
-        var sortedDistances = m_Chunks.Keys.ToList();
-        sortedDistances.Sort((a, b) => Vec2.Manhattan(t, a) - Vec2.Manhattan(t, b));
-
-        var i = sortedDistances.Count - 1;
-        while(m_Chunks.Count > k_MaxChunks) {
-            var farthest = sortedDistances[i];
-            var toRemove = m_Chunks[farthest];
-
-            // do remove stuff here
-            toRemove.gameObject.SetActive(false);
-            // TODO: maybe the pool should also be size limited
-            m_ChunkPool.Enqueue(toRemove);
-            m_Chunks.Remove(farthest);
-
-            i--;
-        }
-    }
-
     // -- c/editor
+    #if UNITY_EDITOR
+    /// reload chunks with new params
     void ReloadEditorChunks() {
         foreach (var (_, chunk) in m_Chunks) {
             chunk.Reload();
         }
     }
 
-    /// create chunks for the editor field
-    void CreateEditorChunks() {
-        #if UNITY_EDITOR
-        // get the editor camera
-        var scene = UnityEditor.SceneView.lastActiveSceneView;
-        if (scene == null) {
-            return;
-        }
-
-        var camera = scene.camera;
-        if (camera == null) {
-            return;
-        }
-
-        // don't create chunks in prefab mode
-        var preview = UnityEditor.SceneManagement.PrefabStageUtility.GetCurrentPrefabStage();
-        if (preview != null) {
-            return;
-        }
-
-        // get the look position and direction
-        var ct = camera.transform;
-        var lp = ct.position;
-        var ld = ct.forward;
-
-        // the ground plane normal (position is always zero) (pretty unclear to me why
-        // the normal is reversed)
-        var pp = Vector3.zero;
-        var pn = Vector3.up;
-
-        // the magnitude of the the look pos to the plane & the look's scale (angle) in the plane
-        var a = Vector3.Dot(pp - lp, pn);
-        var b = Vector3.Dot(ld, pn);
-
-        // get the intersection
-        var intersection = Vector3.zero;
-        if (Mathf.Abs(b) < 0.00001f) {
-            if (Mathf.Abs(a) >= 0.00001f) {
-                return;
-            }
-
-            intersection = lp;
-        } else {
-            intersection = lp + a / b * ld;
-        }
-
-        // set target coordinate
-        var coord = IntoCoordinate(intersection);
-        if (m_TargetCoord != coord) {
-            m_TargetCoord = coord;
-            CreateChunks(3);
-        }
-        #endif
-    }
-
     /// clear all editor chunks
     public void ClearEditorChunks() {
-        #if UNITY_EDITOR
         m_Chunks.Clear();
+        m_ChunkPool.Clear();
 
         // destroy any editor terrain
         var t = transform;
         while (t.childCount > 0) {
             DestroyImmediate(t.GetChild(0).gameObject);
         }
-
-        // reset target coord to sentinel
-        m_TargetCoord = new Vector2Int(69, 420);
-        #endif
     }
-
-    // -- queries --
-    /// finds the chunk coordinate at the position
-    Vector2Int IntoCoordinate(Vector3 pos) {
-        var cs = m_ChunkSize.Value;
-        var ch = cs * 0.5f;
-
-        // get x and y coord for this position
-        var x = Mathf.FloorToInt((pos.x + ch) / cs);
-        var y = Mathf.FloorToInt((pos.z + ch) / cs);
-
-        return new Vector2Int(x, y);
-    }
-
-    /// finds the position of this coordinate
-    Vector3 IntoPosition(Vector2Int coord) {
-        var cs = m_ChunkSize.Value;
-        var ch = cs * 0.5f;
-
-        // get x and z position for this coordinate
-        var x = coord.x * cs - ch;
-        var z = coord.y * cs - ch;
-
-        return new Vector3(x, 0.0f, z);
-    }
+    #endif
 }
