fc80d1c4fcad203e03fbb3244df5e231bb4b62bc
ty cobb <bin@tycobb.club>
2022-10-03 12:28:29 -0400

    add slider script

--- Assets/Menu/Elements/Slider/Slider.cs
+++ Assets/Menu/Elements/Slider/Slider.cs
@@ -1,6 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+namespace Discone {
 
 class Slider: MonoBehaviour {
+    // -- refs --
+    [Header("refs")]
+    [Tooltip("the value label")]
+    [SerializeField] TMP_Text m_ValueLabel;
+
+    // -- events --
+    /// when the value changes
+    public void OnValueChanged(float value) {
+        m_ValueLabel.text = ((int)value).ToString();
+    }
+}
+
 }
