439526430b022000b9a526433698430ab726a62e
ty cobb <bin@tycobb.club>
2022-10-11 11:17:34 -0400

    most of the jump press animation

--- Assets/Plugins/ThirdPerson/CharacterModel.cs
+++ Assets/Plugins/ThirdPerson/CharacterModel.cs
@@ -4,43 +4,91 @@ namespace ThirdPerson {
 
 /// a container for the character's model and animations
 public sealed class CharacterModel: MonoBehaviour {
+    // -- constants --
+    /// the legs animator layer (for yoshiing animation)
+    const string k_LayerLegs = "Legs";
+
+    /// the arms animator layer (for yoshiing animation)
+    const string k_LayerArms = "Arms";
+
+    /// the airborne animator prop
+    const string k_PropIsAirborne = "IsAirborne";
+
+    /// the crouching animator prop
+    const string k_PropIsCrouching = "IsCrouching";
+
+    /// the move speed animator prop
+    const string k_PropMoveSpeed = "MoveSpeed";
+
+    /// the vertical speed animator prop
+    const string k_PropVerticalSpeed = "VerticalSpeed";
+
     // -- fields --
     [Header("parameters")]
-    [SerializeField] private bool AnimateScale;
-    [SerializeField] private float MaxJumpSquatSquash;
-    [SerializeField] private AnimationCurve JumpSquatSquashCurve;
-    [SerializeField] private float VerticalAccelerationStretch;
-    [SerializeField] private AnimationCurve VerticalAccelerationStretchCurve;
-    [SerializeField] private float StretchAndSquashLerp;
-    [SerializeField] private float MinSquashScale = 0;
-    [SerializeField] private float MaxSquashScale = 2;
+    [Tooltip("TODO: leave me a comment")]
+    [SerializeField] bool AnimateScale;
 
-    [Header("references")]
+    [Tooltip("TODO: leave me a comment")]
+    [SerializeField] float MaxJumpSquatSquash;
+
+    [Tooltip("TODO: leave me a comment")]
+    [SerializeField] AnimationCurve JumpSquatSquashCurve;
+
+    [Tooltip("TODO: leave me a comment")]
+    [SerializeField] float VerticalAccelerationStretch;
+
+    [Tooltip("TODO: leave me a comment")]
+    [SerializeField] AnimationCurve VerticalAccelerationStretchCurve;
+
+    [Tooltip("TODO: leave me a comment")]
+    [SerializeField] float StretchAndSquashLerp;
+
+    [Tooltip("TODO: leave me a comment")]
+    [SerializeField] float MinSquashScale = 0;
+
+    [Tooltip("TODO: leave me a comment")]
+    [SerializeField] float MaxSquashScale = 2;
+
+    // -- refs --
+    [Header("refs")]
     [Tooltip("The shared third person animator controller")]
-    [SerializeField] private RuntimeAnimatorController m_AnimatorController;
+    [SerializeField] RuntimeAnimatorController m_AnimatorController;
 
     // -- props --
     /// the character's animator
-    private Animator m_Animator;
+    Animator m_Animator;
+
+    /// the containing character
+    Character m_Container;
 
     /// the character's state
-    private CharacterState m_State;
+    /// TODO: CharacterContainer, CharacterContainerConvertible
+    CharacterState m_State => m_Container.State;
 
     /// the character's tunables
-    private CharacterTunablesBase m_Tunables;
+    /// TODO: CharacterContainer, CharacterContainerConvertible
+    CharacterTunablesBase m_Tunables => m_Container.Tunables;
 
-    /// the initial scale of the character
-    private Vector3 c_BaseScale;
+    /// the character's tunables
+    /// TODO: CharacterContainer, CharacterContainerConvertible
+    CharacterInput m_Input => m_Container.Input;
 
     /// the current strech/squash multiplier
-    private float m_CurrentSquashStretch = 1.0f;
+    float m_CurrentSquashStretch = 1.0f;
+
+    /// the initial scale of the character
+    Vector3 m_InitialScale;
+
+    /// the legs layer index
+    int m_LayerLegs;
+
+    /// the arms layer index
+    int m_LayerArms;
 
     // -- lifecycle --
     void Start() {
         // set dependencies
-        var character = GetComponentInParent<Character>();
-        m_State = character.State;
-        m_Tunables = character.Tunables;
+        m_Container = GetComponentInParent<Character>();
 
         // configure animator
         m_Animator = GetComponentInChildren<Animator>();
@@ -48,8 +96,12 @@ public sealed class CharacterModel: MonoBehaviour {
             m_Animator.runtimeAnimatorController = m_AnimatorController;
         }
 
-        c_BaseScale = transform.localScale;
+        // set props
+        m_InitialScale = transform.localScale;
+        m_LayerLegs = m_Animator.GetLayerIndex(k_LayerLegs);
+        m_LayerArms = m_Animator.GetLayerIndex(k_LayerArms);
 
+        // make sure complex model trees have the correct layer
         gameObject.SetLayerRecursively(gameObject.layer);
     }
 
@@ -70,7 +122,7 @@ public sealed class CharacterModel: MonoBehaviour {
 
         // set move animation params
         anim.SetFloat(
-            "MoveSpeed",
+            k_PropMoveSpeed,
             Mathf.InverseLerp(
                 m_Tunables.Horizontal_MinSpeed,
                 m_Tunables.Horizontal_MaxSpeed,
@@ -78,20 +130,33 @@ public sealed class CharacterModel: MonoBehaviour {
             )
         );
 
-        // set jump animation params
         anim.SetBool(
-            "JumpSquat",
-            m_State.IsInJumpSquat
+            k_PropIsCrouching,
+            m_State.IsInJumpSquat || m_State.IsCrouching
         );
 
+        // set jump animation params
         anim.SetBool(
-            "Airborne",
+            k_PropIsAirborne,
             !m_State.IsGrounded
         );
 
         anim.SetFloat(
-            "VerticalSpeed",
+            k_PropVerticalSpeed,
             m_State.Velocity.y
+        );
+
+        // blend yoshiing
+        // TODO: lerp
+        var yoshiing = !m_State.IsGrounded && m_Input.IsJumpPressed ? 1.0f : 0.0f;
+        anim.SetLayerWeight(
+            m_LayerLegs,
+            yoshiing
+        );
+
+        anim.SetLayerWeight(
+            m_LayerArms,
+            yoshiing
         );
     }
 
@@ -120,7 +185,7 @@ public sealed class CharacterModel: MonoBehaviour {
         targetScale = Mathf.Clamp(targetScale, MinSquashScale, MaxSquashScale);
         m_CurrentSquashStretch = Mathf.Lerp(m_CurrentSquashStretch, targetScale, StretchAndSquashLerp * Time.deltaTime);
 
-        var newScale = c_BaseScale;
+        var newScale = m_InitialScale;
         newScale.y *= m_CurrentSquashStretch;
         transform.localScale = newScale;
     }
