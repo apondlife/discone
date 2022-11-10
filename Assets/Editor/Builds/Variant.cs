c97ad8f82ed046146df2d6e46c52bb37dc4b7ee2
mutmedia <gustavo.ceci95@gmail.com>
2022-11-09 22:43:00 -0500

    art gallery

--- Assets/Editor/Builds/Variant.cs
+++ Assets/Editor/Builds/Variant.cs
@@ -9,10 +9,14 @@ public static class Variant {
     /// the playtest variant
     public const string Playtest = "playtest";
 
+    /// the playtest variant
+    public const string Art = "art";
+
     // -- factories --
     /// decode the variant from a raw arg
     public static string Decode(string variant) {
         return variant switch {
+            Art      => Art,
             Playtest => Playtest,
             Release  => Release,
             ""       => Release,
