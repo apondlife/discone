e2bdae185a1c1bd6f17f3d3bc11cf7b276c27e9b
iatenothingbutriceforthreedays <xxx@x.com>
2023-08-08 21:12:49 +0900

    simplify editor for SimpleCharacterMusic; replace schwing sounds with new guitar audio for step sounds

--- Assets/Audio/Character/SimpleCharacterMusic.cs
+++ Assets/Audio/Character/SimpleCharacterMusic.cs
@@ -2,18 +2,21 @@ using Musicker;
 using ThirdPerson;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
+using FMODUnity;
 
-
-[RequireComponent(typeof(FMODUnity.StudioEventEmitter))]
 public sealed class SimpleCharacterMusic: CharacterMusicBase {
     // -- refs --
     [Header("refs")]
-    [Tooltip("the fmod event emitter for continuous character sounds")]
-    [SerializeField] FMODUnity.StudioEventEmitter m_ContinuousEmitter;
-    [Tooltip("the fmod event emitter for steps")]
-    [SerializeField] FMODUnity.StudioEventEmitter m_JumpEmitter;
-    [Tooltip("the fmod event emitter for jumps")]
-    [SerializeField] FMODUnity.StudioEventEmitter m_StepEmitter;
+    [Tooltip("the fmod event for continuous character sounds")]
+    [SerializeField] EventReference m_Continuous;
+    [Tooltip("the fmod event for jumps")]
+    [SerializeField] EventReference m_Jump;
+    [Tooltip("the fmod event for steps")]
+    [SerializeField] EventReference m_Step;
+
+    StudioEventEmitter m_ContinuousEmitter;
+    StudioEventEmitter m_JumpEmitter;
+    StudioEventEmitter m_StepEmitter;
 
     // these should probably all just be somewhere shared (charactermusicbase?)
     static readonly string k_ParamSpeed= "Speed";  // float, 0 to ~20
@@ -34,6 +37,13 @@ public sealed class SimpleCharacterMusic: CharacterMusicBase {
         m_Container.Character.Events.Bind(CharacterEvent.Jump, PlayJump);
         m_Container.OnSimulationChanged += OnSimulationChanged;
 
+        // add emitters
+        m_StepEmitter = gameObject.AddComponent<StudioEventEmitter>();
+        m_StepEmitter.EventReference = m_Step;
+        m_JumpEmitter = gameObject.AddComponent<StudioEventEmitter>();
+        m_JumpEmitter.EventReference = m_Jump;
+        m_ContinuousEmitter = gameObject.AddComponent<StudioEventEmitter>();
+        m_ContinuousEmitter.EventReference = m_Continuous;
         m_ContinuousEmitter.Play();
     }
     public override void OnStep(int foot, bool isRunning) {
@@ -69,11 +79,12 @@ public sealed class SimpleCharacterMusic: CharacterMusicBase {
 
     void PlayStep() {
         // do pitch quantization here because it's much harder to do in fmod
-        int[] pitches = {-5, -3, -3, 0, 2, 3, 5};
+        int[] pitches = {-7, -7, -7, -7, -5, -5, 0, 2, 4, 5, 7, 7, 7};
         int i = (int)(Mathf.InverseLerp(-1f, 1f, Slope)*pitches.Length);
         float pitch = (float)pitches[i];
         // Debug.Log(pitch);
         FMODParams ps = CurrentFmodParams;
+        Debug.Log(pitch);
         ps[k_ParamPitch] = pitch;
         FMODPlayer.PlayEvent(new FMODEvent (m_StepEmitter, ps));
     }
