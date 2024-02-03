7967a8c6c7d0d435facd4dd473f6e6720e48cd2a
ty cobb <bin@tycobb.club>
2024-02-02 20:10:01 -0500

    reduce speedline lifetime on direction change

--- Assets/Plugins/ThirdPerson/Runtime/Effects/SpeedLines/CharacterSpeedLines.cs
+++ Assets/Plugins/ThirdPerson/Runtime/Effects/SpeedLines/CharacterSpeedLines.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ThirdPerson {
 
@@ -9,7 +10,7 @@ sealed class CharacterSpeedLines: MonoBehaviour {
     [Tooltip("the scale for start speed as a fn of sqr velocity")]
     [SerializeField] float m_SpeedScale;
 
-    [Tooltip("the scale for rotation over time as a fn acceleration")]
+    [Tooltip("the scale for rotation over time as a fn of acceleration")]
     [SerializeField] Vector2 m_RotationScale;
 
     [Tooltip("the max speed change per second")]
@@ -21,26 +22,35 @@ sealed class CharacterSpeedLines: MonoBehaviour {
     [Tooltip("the max rotation change per second")]
     [SerializeField] float m_MaxRotationDelta;
 
+    [Tooltip("the lifetime multiplier as a fn of direction change")]
+    [SerializeField] MapOutCurve m_DirectionScale;
+
     // -- refs --
     [Header("refs")]
     [Tooltip("the anchor transform")]
     [SerializeField] Transform m_Anchor;
 
+    [FormerlySerializedAs("m_Particles")]
     [Tooltip("the particle that shows horizontal speed")]
-    [SerializeField] ParticleSystem m_Particles;
+    [SerializeField] ParticleSystem m_System;
 
     // -- props --
     /// the character container
     CharacterContainer c;
+
+    /// a buffer for particles
+    ParticleSystem.Particle[] m_Particles;
 
     // -- lifecycle --
     void Start() {
         // set deps
         this.c = GetComponentInParent<CharacterContainer>();
 
-        // set initial state
-        var emission = m_Particles.emission;
-        emission.rateOverTime = 0.0f;
+        // set props
+        m_Particles = new ParticleSystem.Particle[m_System.main.maxParticles];
+    }
+
+    void LateUpdate() {
     }
 
     void FixedUpdate() {
@@ -53,7 +63,7 @@ sealed class CharacterSpeedLines: MonoBehaviour {
         var dir = v.normalized;
 
         // scale speed line based on ground speed
-        var main = m_Particles.main;
+        var main = m_System.main;
 
         var destSpeed = v.sqrMagnitude * m_SpeedScale;
         var nextSpeed = Mathf.MoveTowards(
@@ -64,7 +74,7 @@ sealed class CharacterSpeedLines: MonoBehaviour {
 
         main.startSpeed = nextSpeed;
 
-        // rotate speed line emitter opposite movement
+        // rotate emitter to oppose movement
         if (v != Vector3.zero) {
             transform.forward = Vector3.RotateTowards(
                 transform.forward,
@@ -75,7 +85,7 @@ sealed class CharacterSpeedLines: MonoBehaviour {
         }
 
         // rotate lines as character accelerates
-        var vol = m_Particles.velocityOverLifetime;
+        var vol = m_System.velocityOverLifetime;
 
         var a = transform.InverseTransformVector(c.State.Curr.Acceleration);
         var destOrbital = new Vector2(a.y, -a.x) * m_RotationScale * m_SpeedScale;
@@ -91,6 +101,26 @@ sealed class CharacterSpeedLines: MonoBehaviour {
         vol.orbitalX = nextOrbital.x;
         vol.orbitalY = nextOrbital.y;
         vol.orbitalZ = 1f;
+
+        // reduce the lifetime of particles on direction change
+        var n = m_System.GetParticles(m_Particles, m_Particles.Length);
+        if (n != 0) {
+            for (var i = 0; i < n; i++) {
+                var particle = m_Particles[i];
+
+                var dirAlignment = Mathf.Max(0f, -Vector3.Dot(
+                    dir,
+                    particle.totalVelocity.normalized
+                ));
+
+                var dirScale = m_DirectionScale.Evaluate(dirAlignment);
+                particle.remainingLifetime *= dirScale;
+
+                m_Particles[i] = particle;
+            }
+
+            m_System.SetParticles(m_Particles, n);
+        }
     }
 }
 
