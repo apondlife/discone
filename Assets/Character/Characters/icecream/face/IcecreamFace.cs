7928708e7aa9c76bff7509fb7be81f615fa2ee45
mutmedia <gustavo.ceci95@gmail.com>
2024-02-15 15:00:23 -0500

    fixed icecream blinking

--- Assets/Character/Characters/icecream/face/IcecreamFace.cs
+++ Assets/Character/Characters/icecream/face/IcecreamFace.cs
@@ -10,24 +10,31 @@ public class IcecreamFace : MonoBehaviour
     /// index for the neutral face (i.e. open eyes)
     const int k_NeutralFaceSpriteIndex = 0;
 
+    // -- cfg --
     [Header("config")]
     [Tooltip("the time it takes for a blink")]
     [SerializeField] float m_BlinkTime;
 
     [Tooltip("minimum time between blinks (in seconds)")]
     [SerializeField] float m_MinTimeBetweenBlinks;
-    
+
     [Tooltip("maximum time between blinks (in seconds)")]
     [SerializeField] float m_MaxTimeBetweenBlinks;
-    
-    [Header("refs")]
-    [SerializeField] Renderer m_Renderer;
 
-    // index for the current sprite in the blink animation
+    // -- props --
+    /// the renderer
+    Renderer m_Renderer;
+
+    /// index for the current sprite in the blink animation
     int m_CurrentSpriteIndex = 0;
 
     /// the blink animation
     Coroutine m_Blink;
+
+    // -- lifecycle --
+    void Awake() {
+        m_Renderer = GetComponent<Renderer>();
+    }
 
     void Start()
     {
@@ -35,24 +42,22 @@ public class IcecreamFace : MonoBehaviour
 
         BlinkAfterRandomInterval();
     }
-    // TODO: make fun blink
-    void Update()
-    {
-        m_Renderer.material.SetInteger(ShaderProps.CurrentSprite, m_CurrentSpriteIndex);
-    }
 
+    // -- commands --
     void BlinkAfterRandomInterval()  {
         float interval = Random.Range(m_MinTimeBetweenBlinks, m_MaxTimeBetweenBlinks);
-        this.DoAfterTime(interval, DoBlink);
+        this.DoAfterTime(interval, Blink);
     }
-    
-    void DoBlink() {
+
+    // TODO: make fun blink (alternating eyes)
+    void Blink() {
         m_Blink = StartCoroutine(CoroutineHelpers.InterpolateByTime(
             m_BlinkTime,
 
             (k) => {
                 float mapped = Mathf.Lerp(0, k_NumBlinkSprites, k);
                 m_CurrentSpriteIndex = Mathf.FloorToInt(mapped);
+                m_Renderer.material.SetInteger(ShaderProps.CurrentSprite, m_CurrentSpriteIndex);
             },
 
             () => {
