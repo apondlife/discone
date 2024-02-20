81a7f11472abd0dff5eda9dfa4e710fe4e825224
mutmedia <gustavo.ceci95@gmail.com>
2024-02-20 17:48:11 -0500

    created dream sequence steps

---
+++ Assets/World/Dream/DreamSequence.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UnityAtoms;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Yarn.Unity;
+
+namespace Discone {
+
+sealed class DreamSequence: MonoBehaviour {
+    [Serializable]
+    record Step {
+        [Tooltip("the step's trigger")]
+        public DreamSequenceTrigger Trigger;
+
+        [Tooltip("the mechanic node to play on start")]
+        [YarnNode(nameof(m_Mechanic))]
+        public string Mechanic_StartNode;
+    }
+
+    // -- cfg --
+    [Header("cfg")]
+    [Tooltip("the sequence of steps")]
+    [SerializeField] Step[] m_Steps;
+
+    // -- refs --
+    [Header("refs")]
+    [FormerlySerializedAs("m_FlowerPosition")]
+    [Tooltip("the initial flower position")]
+    [SerializeField] Transform m_InitialFlowerPos;
+
+    [Tooltip("a reference to the current character")]
+    [SerializeField] DisconeCharacterVariable m_CurrentCharacter;
+
+    [Tooltip("the shared data store")]
+    [SerializeField] Store m_Store;
+
+    [Tooltip("the mechanic yarn project")]
+    [SerializeField] YarnProject m_Mechanic;
+
+    // -- dispatched --
+    [Header("dispatched")]
+    [Tooltip("when the mechanic should jump to a node")]
+    [SerializeField] StringEvent m_Mechanic_JumpToNode;
+
+    /// -- props --
+    /// the current step in the sequence
+    int m_StepIndex;
+
+    /// the set of event subscriptions
+    readonly DisposeBag m_Subscriptions = new();
+
+    /// -- lifecycle --
+    void Start() {
+        m_Subscriptions
+            .Add(m_Store.LoadFinished, OnLoadFinished)
+            .Add(m_CurrentCharacter.ChangedWithHistory, OnCurrentCharacterChanged);
+
+        var i = 0;
+        foreach (var step in m_Steps) {
+            step.Trigger.OnFire(OnStep);
+            step.Trigger.gameObject.SetActive(i == 0);
+            i += 1;
+        }
+    }
+
+    /// -- events --
+    void OnLoadFinished() {
+        Debug.Log($"[dream] OnLoadFinished {m_Store.Player.HasData}");
+    }
+
+    void OnCurrentCharacterChanged(DisconeCharacterPair _) {
+        Debug.Log("[dream] OnCharacterChanged");
+        if(m_Store.Player.HasData) {
+            DestroyImmediate(this);
+            return;
+        }
+
+        // TODO: DisconeCharacterReference
+        m_CurrentCharacter.Value.PlantFlower(Checkpoint.FromTransform(m_InitialFlowerPos));
+    }
+
+    void OnStep() {
+        var curr = m_Steps[m_StepIndex];
+        Destroy(curr.Trigger.gameObject);
+
+        m_Mechanic_JumpToNode.Raise(curr.Mechanic_StartNode);
+        m_StepIndex += 1;
+
+        var next = m_Steps[m_StepIndex];
+        next.Trigger.gameObject.SetActive(true);
+    }
+}
+
+}
---
+++ Assets/World/Dream/DreamSequenceTrigger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.Remoting.Services;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Discone {
+
+[RequireComponent(typeof(Collider))]
+sealed class DreamSequenceTrigger: MonoBehaviour {
+    enum Event {
+        Enter,
+        Stay,
+        Exit,
+    }
+
+    // -- cfg --
+    [Header("cfg")]
+    [Tooltip("the kind of event to trigger")]
+    [SerializeField] Event m_Event;
+
+    [Tooltip("the duration for the stay event to fire")]
+    [ShowIf(nameof(m_Event), Event.Stay)]
+    [SerializeField] float m_StayDuration;
+
+    // -- props --
+    /// an action to to fire on trigger enter
+    Action m_OnFire;
+
+    /// an action to to fire on trigger enter
+    float m_StayElapsed;
+
+    // -- commands --
+    /// add a listener to the trigger's enter event
+    public void OnFire(Action action) {
+        m_OnFire += action;
+    }
+
+    void Fire() {
+        m_OnFire?.Invoke();
+    }
+
+    // -- queries --
+    /// if the collider is the current player
+    static bool IsPlayer(Collider other) {
+        var player = other.GetComponentInParent<DisconePlayer>();
+        return player && player.Character;
+    }
+
+    // -- events --
+    void OnTriggerEnter(Collider other) {
+        if (!IsPlayer(other)) {
+            return;
+        }
+
+        if (m_Event != Event.Enter) {
+            return;
+        }
+
+        Fire();
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (!IsPlayer(other)) {
+            return;
+        }
+
+        if (m_Event != Event.Stay) {
+            return;
+        }
+
+        m_StayElapsed += Time.deltaTime;
+        if (m_StayElapsed >= m_StayDuration) {
+            Fire();
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (!IsPlayer(other)) {
+            return;
+        }
+
+        if (m_Event == Event.Stay) {
+            m_StayElapsed = 0f;
+        }
+
+        if (m_Event != Event.Exit) {
+            return;
+        }
+
+        Fire();
+    }
+}
+
+}
